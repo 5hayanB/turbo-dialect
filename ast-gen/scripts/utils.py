6634b00c1e5a8c6f2f22bfea9388b5c91e1411b4
5hayanB <baig.sh@tutanota.com>
2024-02-08 16:48:39 +0500

    temp

--- ast-gen/scripts/utils.py
+++ ast-gen/scripts/utils.py
@@ -7,13 +7,24 @@ def dump_compressed_ast(compressed_ast, dest):
             formatted_compressed_ast += char
             indent += 4
             formatted_compressed_ast += f'\n{" " * indent}'
-        if char == ',':
+        elif char == ',':
             formatted_compressed_ast += char
             formatted_compressed_ast += f'\n{" " * indent}'
-        if char == '}':
+        elif char == '}':
             indent -= 4
             formatted_compressed_ast += f'\n{" " * indent}'
             formatted_compressed_ast += char
+            # formatted_compressed_ast += f'\n'
+        else:
+            formatted_compressed_ast += char
+        # print(f'{formatted_compressed_ast = }')
 
-    with open(dest, 'r', encoding='UTF-8') as f:
+    with open(dest, 'w', encoding='UTF-8') as f:
         f.write(formatted_compressed_ast)
+
+
+if __name__ == '__main__':
+    with open('/home/shayan/repositories/my_repositories/github/turbo-dialect/ast-gen/tests/compression/generated_ast2.txt', 'r', encoding='UTF-8') as f:
+        compressed_ast = f.read()
+    dump_compressed_ast(compressed_ast,
+                        '/home/shayan/repositories/my_repositories/github/turbo-dialect/ast-gen/tests/compression/compressed2.txt')
