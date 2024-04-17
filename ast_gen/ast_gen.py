b20a105ba6feb3ed9efc69c2c8b8bb8888803421
5hayanB <baig.sh@tutanota.com>
2024-04-17 16:44:45 +0500

    temp commit

--- ast_gen/ast_gen.py
+++ ast_gen/ast_gen.py
@@ -7,10 +7,6 @@ from models import MODELS
 GRAMMAR_FILE = 'ast_gen/grammar/compressed_ast.gbnf'
 GRAMMAR = LlamaGrammar.from_file(file=GRAMMAR_FILE)
 
-# load system prompts
-with open('ast_gen/prompts/role.txt', 'r', encoding='utf-8') as f:
-    ROLE = f.read()
-
 # load model
 # MODEL = MODELS['mistral-7b-v0.1.Q8']
 # LLM = Llama(model_path=MODEL['path'], n_ctx=MODEL['n_ctx'], n_gpu_layers=-1)
@@ -18,14 +14,17 @@ MODEL = MODELS['openhermes-2.5-mistral-7b-16k.Q8']
 LLM = Llama(model_path=MODEL['path'], n_ctx=MODEL['n_ctx'], n_gpu_layers=-1, chat_format='chatml')
 
 
-def get_ast(prompt, template_file, description_file, grammar_file):
+def get_ast(prompt, grammar_file, role_file, template_file, description_file):
+    grammar = LlamaGrammar.from_file(file=grammar_file)
+
+    with open(role_file, 'r', encoding='utf-8') as f:
+        role = f.read()
     with open(template_file, 'r', encoding='utf-8') as f:
         template = f.read()
     with open(description_file, 'r', encoding='utf-8') as f:
         description = f.read()
-    sys_prompt = f'{ROLE}{template}{description}'
+    sys_prompt = f'{role}{template}{description}'
 
-    grammar = LlamaGrammar.from_file(file=grammar_file)
     # response = LLM.create_completion(
     #     SYSTEM_PROMPT + prompt,
     #     grammar=GRAMMAR,
@@ -46,9 +45,20 @@ def get_ast(prompt, template_file, description_file, grammar_file):
 
 
 def create_parse_tree(prompt):
-    top_module_ast = get_ast(prompt, 'ast_gen/prompts/top_module_ast_template.txt', 'ast_gen/prompts/top_module_ast_description.txt', 'ast_gen/grammar/top_module_ast.gbnf')
+    top_module_ast = get_ast(
+        prompt,
+        grammar_file='ast_gen/grammar/top_module_ast.gbnf',
+        role_file='ast_gen/prompts/top_module_role.txt',
+        template_file='ast_gen/prompts/top_module_ast_template.txt',
+        description_file='ast_gen/prompts/top_module_ast_description.txt'
+    )
+    io_ast = get_ast(
+        prompt,
+        grammar_file='ast_gen/grammar/io_ast.gbnf',
+        role_file='./',
+        template_file='./prompts/io_ast_template.txt',
+        description_file='./prompts/io_ast_description.txt'
+    )
     print(f'{top_module_ast = }')
-
-    io_ast = get_ast(prompt, 'ast_gen/prompts/io_ast_template.txt', 'ast_gen/prompts/io_ast_description.txt', 'ast_gen/grammar/io_ast.gbnf')
     print(f'{io_ast = }')
 
