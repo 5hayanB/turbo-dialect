4fb2f69663abcce9474dbd56aa22199083bef9c6
5hayanB <baig.sh@tutanota.com>
2024-04-17 14:53:14 +0500

    temp commit

--- cmdline_main.py
+++ cmdline_main.py
@@ -8,6 +8,6 @@ def get_prompt(file):
 
 
 if __name__ == '__main__':
-    prompt = get_prompt('./tests/input_prompts/4x4_systolic_array_dataflow.txt')
+    prompt = get_prompt('./tests/input_prompts/4x4_systolic_array_rtl.txt')
     create_parse_tree(prompt)
 
