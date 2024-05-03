6d2617db34e0b9047e7410f1d5e47246c2bc309c
5hayanB <baig.sh@tutanota.com>
2024-05-03 07:17:54 +0500

    temp commit

--- ast_gen/ast_gen.py
+++ ast_gen/ast_gen.py
@@ -13,16 +13,17 @@ LLM = Llama(
 )
 
 
-def get_ast(prompt, grammar_file, role_file, template_file, description_file):
+def get_ast(prompt, grammar_file, role_file, description_file):
     grammar = LlamaGrammar.from_file(file=grammar_file)
 
     with open(role_file, 'r', encoding='utf-8') as f:
         role = f.read()
-    with open(template_file, 'r', encoding='utf-8') as f:
-        template = f.read()
-    with open(description_file, 'r', encoding='utf-8') as f:
-        description = f.read()
-    sys_prompt = f'{role}{template}{description}'
+    # with open(template_file, 'r', encoding='utf-8') as f:
+    #     template = f.read()
+    # with open(description_file, 'r', encoding='utf-8') as f:
+    #     description = f.read()
+    # sys_prompt = f'{role}{template}{description}'
+    sys_prompt = f'{role}{description_file}'
 
     while True:
         response = LLM.create_chat_completion(
@@ -40,28 +41,36 @@ def get_ast(prompt, grammar_file, role_file, template_file, description_file):
 
 
 def create_parse_tree(prompt):
-    top_module_ast = get_ast(
+    # top_module_ast = get_ast(
+    #     prompt,
+    #     grammar_file='ast_gen/grammar/top_module_ast.gbnf',
+    #     role_file='ast_gen/prompts/top_module_role.txt',
+    #     template_file='ast_gen/prompts/top_module_ast_template.txt',
+    #     description_file='ast_gen/prompts/top_module_ast_description.txt'
+    # )
+    # io_ast = get_ast(
+    #     prompt,
+    #     grammar_file='ast_gen/grammar/io_ast.gbnf',
+    #     role_file='ast_gen/prompts/io_role.txt',
+    #     template_file='ast_gen/prompts/io_ast_template.txt',
+    #     description_file='ast_gen/prompts/io_ast_description.txt'
+    # )
+    # wire_ast = get_ast(
+    #     prompt,
+    #     grammar_file='ast_gen/grammar/cells_ast.gbnf',
+    #     role_file='ast_gen/prompts/cells_role.txt',
+    #     template_file='ast_gen/prompts/cells_ast_template.txt',
+    #     description_file='ast_gen/prompts/cells_ast_description.txt'
+    # )
+    # print(f'{top_module_ast = }')
+    # print(f'{io_ast = }')
+    # print(f'{wire_ast = }')
+
+    vulcan_code = get_ast(
         prompt,
-        grammar_file='ast_gen/grammar/top_module_ast.gbnf',
-        role_file='ast_gen/prompts/top_module_role.txt',
-        template_file='ast_gen/prompts/top_module_ast_template.txt',
-        description_file='ast_gen/prompts/top_module_ast_description.txt'
+        grammar_file='ast_gen/grammar/vulcan.gbnf',
+        role_file='ast_gen/prompts/role.txt',
+        description_file='ast_gen/prompts/vulcan.txt'
     )
-    io_ast = get_ast(
-        prompt,
-        grammar_file='ast_gen/grammar/io_ast.gbnf',
-        role_file='ast_gen/prompts/io_role.txt',
-        template_file='ast_gen/prompts/io_ast_template.txt',
-        description_file='ast_gen/prompts/io_ast_description.txt'
-    )
-    wire_ast = get_ast(
-        prompt,
-        grammar_file='ast_gen/grammar/cells_ast.gbnf',
-        role_file='ast_gen/prompts/cells_role.txt',
-        template_file='ast_gen/prompts/cells_ast_template.txt',
-        description_file='ast_gen/prompts/cells_ast_description.txt'
-    )
-    print(f'{top_module_ast = }')
-    print(f'{io_ast = }')
-    print(f'{wire_ast = }')
+    print(f'{vulcan_code = }')
 
