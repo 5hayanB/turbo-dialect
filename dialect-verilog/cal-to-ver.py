d09d980bcd2a81e599fd2827b262669a36887422
MuhammadTalha-10 <mt066661@gmail.com>
2024-05-06 13:48:32 +0500

    cal-to-val

--- dialect-verilog/cal-to-ver.py
+++ dialect-verilog/cal-to-ver.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 import subprocess
-from importlib import import_module
+# from importlib import import_module
 
-imp_file = import_module ("../ast-dialect/ast_conversion")
+# imp_file = import_module ("ast-dialect.ast_conversion.convert_to_calyx")
+
+import sys
+sys.path.insert(0, '/home/talha/turbo-dialect/ast-dialect/ast_conversion.convert_to_calyx')
+
 
 def convert_futil_to_verilog(futil_file_path):
-    command = f"fud e {imp_file.convert_to_calyx} --to verilog --from calyx"
+    command = f"fud e tests/verilog/untitled.futil --to verilog --from calyx"
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     return result.stdout
 
