292752d2549fb7c3ebdee9beb2b0f4e9456507af
5hayanB <baig.sh@tutanota.com>
2024-06-06 13:49:11 +0500

    temp commit

--- ast_gen/ast_gen.py
+++ ast_gen/ast_gen.py
@@ -9,10 +9,8 @@ MODEL = MODELS['meta-llama-3-8b-instruct']
 LLM = Llama(model_path=MODEL['path'], n_ctx=MODEL['n_ctx'], n_gpu_layers=MODEL['n_gpu_layers'])
 
 
-def llm_response(sys_prompt_dir, sys_prompt_file, user_prompt, debug,
-                 debug_msg='', grammar=None):
-    with open(os.path.join(sys_prompt_dir, sys_prompt_file), 'r', encoding='utf-8') as f:
-        sys_prompt = f'ROLE:\n{f.read()}'
+def llm_response(sys_prompt, user_prompt, debug, debug_msg='',
+                 grammar=None):
     while True:
         if grammar:
             response = LLM.create_chat_completion(
@@ -34,56 +32,67 @@ def llm_response(sys_prompt_dir, sys_prompt_file, user_prompt, debug,
     return response['choices'][0]['message']['content']
 
 
-def conclusion_start(llm_response)
+def retrieve_conclusion(response, debug):
+    sys_prompt_file = os.path.join('ast_gen', 'prompts', 'helpers', 'ensure_conclusion.txt')
+    with open(sys_prompt_file, 'r', encoding='utf-8') as f:
+        sys_prompt = f.read()
+    while True:
+        if 'CONCLUSION' in response:
+            conclusion_pos = response.index('CONCLUSION') + 9
+            # Consume non-whitespace characters
+            while re.match(r'\S', response[conclusion_pos+1]):
+                conclusion_pos += 1
+            # Consume whitespace characters
+            while re.match(r'\s', response[conclusion_pos+1]):
+                conclusion_pos += 1
+            if debug:
+                print(f'\nconclusion:\n{response[conclusion_pos+1:]}')
+            return response[conclusion_pos+1:]
+        else:
+            response = f'EXPLANATION:\n{response}'
+            response = llm_response(sys_prompt, response, debug, 'ensure_conclusion')
 
 
 def extract_module_name(prompt, debug):
     LLM.reset()
     prompts_dir = os.path.join('ast_gen', 'prompts', 'module_name')
-    sys_prompt_file = 'extract_name.txt'
+    sys_prompt_file = os.path.join(prompts_dir, 'extract_name.txt')
     while True:
-        module_name = llm_response(prompts_dir, sys_prompt_file, prompt, debug,
-                                   'module_name')
+        with open(sys_prompt_file, 'r', encoding='utf-8') as f:
+            sys_prompt = f'ROLE:\n{f.read()}'
+        module_name = llm_response(sys_prompt, prompt, debug, 'module_name')
         module_name = re.findall('"[a-zA-Z_][a-zA-Z_0-9]*"', module_name)
         if module_name:
             break
         else:
-            sys_prompt_file = 'name_inclusion.txt'
+            sys_prompt_file = os.path.join(prompts_dir, 'name_inclusion.txt')
     return module_name[0].strip('"')
 
 
-def extract_input_ports(prompt, debug):
+def extract_ports(prompt, ports, debug):
     LLM.reset()
-    prompts_dir = os.path.join('ast_gen', 'prompts', 'inputs')
-    # Identify inputs
-    sys_prompt_file = 'identify_inputs.txt'
-    while True:
-        response = llm_response(prompts_dir, sys_prompt_file, prompt, debug,
-                                'identified_inputs')
-        if 'CONCLUSION' in response:
-            conclusion_pos = response.index('\nCONCLUSION:\n') + 12
-            conclusion = response[conclusion_pos:]
-            break
-        else:
-            sys_prompt_file = 'inputs_conclusion.txt'
-    identified_inputs = f'IDENTIFIED INPUT PORTS:{conclusion}'
-    # Check grouping
-    sys_prompt_file = 'check_input_grouping.txt'
-    while True:
-        response = llm_response(prompts_dir, sys_prompt_file, identified_inputs, debug,
-                                'input_grouping_check')
-        if 'CONCLUSION' in response:
-            conclusion_pos = response.index('\nCONCLUSION:\n') + 12
-            conclusion = response[conclusion_pos:]
-            break
-        else:
-            sys_prompt_file = 'inputs_conclusion.txt'
-    input_grouping_check = f'IDENTIFIED INPUT PORTS:\n{conclusion}'
+    prompts_dir = os.path.join('ast_gen', 'prompts', ports)
+    # Identify ports
+    sys_prompt_file = os.path.join(prompts_dir, f'identify_{ports}.txt')
+    with open(sys_prompt_file, 'r', encoding='utf-8') as f:
+        sys_prompt = f'ROLE:{f.read()}'
+    response = llm_response(sys_prompt, prompt, debug, f'identified_{ports}')
+    conclusion = retrieve_conclusion(response, debug)
+    identified_ports = f'IDENTIFIED {ports[: -1].upper()} PORTS:\n{conclusion}\n'
+    # Indicate grouping
+    sys_prompt_file = os.path.join(prompts_dir, f'indicate_{ports}_grouping.txt')
+    with open(sys_prompt_file, 'r', encoding='utf-8') as f:
+        sys_prompt = f'ROLE:\n{f.read()}'
+    response = llm_response(sys_prompt, identified_ports, debug, f'{ports}_grouping_indication')
+    conclusion = retrieve_conclusion(response, debug)
+    port_grouping_indication = f'GROUPING INDICATION:\n{conclusion}\n'
     # Assign variables
-    response = llm_response(prompts_dir, 'assign_input_variables.txt', identified_inputs+input_grouping_check, debug,
-                            'assign_input_variables')
-    # # input_ports = str(re.findall(r'[a-zA-Z_][a-zA-Z0-9_]*(?:\[\d+\])?:\d+', response))
-    # assign_input_variables = f'INPUT PORTS:\n{response}'
+    sys_prompt_file = os.path.join(prompts_dir, f'assign_{ports}_variables.txt')
+    with open(sys_prompt_file, 'r', encoding='utf-8') as f:
+        sys_prompt = f'ROLE:\n{f.read()}'
+    response = llm_response(sys_prompt, identified_ports+port_grouping_indication, debug, f'assign_{ports}_variables')
+    # input_ports = str(re.findall(r'[a-zA-Z_][a-zA-Z0-9_]*(?:\[\d+\])?:\d+', response))
+    assign_input_variables = f'INPUT PORTS:\n{response}'
     # # Input ports
     # sys_prompt_file = 'input_ports.txt'
     # while True:
@@ -100,50 +109,11 @@ def extract_input_ports(prompt, debug):
     # return input_ports
 
 
-def extract_output_ports(prompt, debug):
-    LLM.reset()
-    prompts_dir = os.path.join('ast_gen', 'prompts', 'outputs')
-    # Identify outputs
-    sys_prompt_file = 'identify_outputs.txt'
-    while True:
-        response = llm_response(prompts_dir, sys_prompt_file, prompt, debug,
-                                'identified_outputs')
-        if 'CONCLUSION' in response:
-            conclusion_pos = response.index('\nCONCLUSION:\n') + 12
-            conclusion = response[conclusion_pos:]
-            break
-        else:
-            sys_prompt_file = 'outputs_conclusion.txt'
-    identified_outputs = f'IDENTIFIED OUTPUT PORTS:{conclusion}'
-    # Check grouping
-    # response = llm_response(prompts_dir, 'check_output_grouping.txt', identified_outputs, debug,
-    #                         'output_grouping_check')
-    # output_grouping_check = f'IDENTIFIED OUTPUT PORTS:\n{response}'
-    # # Assign variables
-    # response = llm_response(prompts_dir, 'assign_output_variables.txt', output_grouping_check, debug,
-    #                         'assign_output_variables')
-    # assign_output_variables = f'OUTPUT PORTS:\n{response}'
-    # # Output ports
-    # sys_prompt_file = 'output_ports.txt'
-    # while True:
-    #     grammar = LlamaGrammar.from_file(file=os.path.join('ast_gen', 'grammar', 'vulcan_ports.gbnf'))
-    #     output_ports = llm_response(prompts_dir, sys_prompt_file, assign_output_variables, debug,
-    #                                'output_ports', grammar)
-    #     # Check outputs
-    #     check_outputs = f'{assign_output_variables}\nGENERATED OUTPUT PORTS:\n{output_ports}'
-    #     response = llm_response(prompts_dir, 'check_outputs.txt', check_outputs, debug,
-    #                             'check_outputs').lower()
-    #     if 'true' in response:
-    #         break
-    #     sys_prompt_file = 'outputs_correction.txt'
-    # return output_ports
-
-
 def create_vulcan_module(prompt, debug):
     circuit_description = f'CIRCUIT DATAFLOW DESCRIPTION:\n{prompt}'
-    module_name = extract_module_name(circuit_description, debug=debug)
-    inputs = extract_input_ports(circuit_description, debug=debug)
-    # outputs = extract_output_ports(circuit_description, debug=debug)
+    module_name = extract_module_name(circuit_description, debug)
+    inputs = extract_ports(circuit_description, 'inputs', debug)
+    # outputs = extract_ports(circuit_description, 'outputs', debug)
     if debug:
         print(f'{module_name = }')
         print(f'{inputs = }')
